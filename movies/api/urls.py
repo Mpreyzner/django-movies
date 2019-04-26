1110be087ec1a3b0f807eb6ee0dd274960db6e6b
Mpreyzner <maria.preyzner@gmail.com>
2019-04-26 20:51:34 +0200

    Added proper routes

---
+++ movies/api/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+
+try:
+    from django.conf.urls import patterns
+except ImportError:
+    pass
+from . import views
+
+urlpatterns = [
+    url(r'^movies/$', views.MovieAPIListView.as_view()),
+    url(r'^comments/$', views.CommentAPIListView.as_view()),
+    url(r'^top/$', views.CommentAPIListView.as_view()),
+
+]
